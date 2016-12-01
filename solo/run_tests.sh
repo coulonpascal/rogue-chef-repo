c2ce4c5ff4a49413791a9dc0cd6642aa71b018ae
coulon_p <pascal.coulon@scisys.co.uk>
2016-12-01 14:02:40 +0000

    update git repo

--- solo/run_tests.sh
+++ solo/run_tests.sh
@@ -5,5 +5,5 @@ set -e
 
 TMP_LOCATION=/tmp/rogue-cookbook
 rm -rf $TMP_LOCATION
-git clone --depth=1 --branch=master https://github.com/ROGUE-JCTD/rogue-cookbook.git $TMP_LOCATION
+git clone --depth=1 --branch=master https://github.com/coulonpascal/rogue-chef-repo.git $TMP_LOCATION
 rspec $TMP_LOCATION/test/integration/default/serverspec
